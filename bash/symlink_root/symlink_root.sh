775ea056ab4d7f1355ad32d6d9640777332e8f48
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-02-05 20:39:06 +0100

    bash symlink_root init commit

---
+++ bash/symlink_root/symlink_root.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+############################################
+# This script creates symlinks 
+# in the root directory.
+# It helps to nagivate in the system.
+############################################
+
+cd /
+sudo ln -s /home/jesmasta/git git
+sudo ln -s /home/jesmasta/Dropbox Dropbox
+sudo ln -s /home/jesmasta/git/chartula chartula
+sudo ln -s /home/jesmasta/amu amu
+sudo ln -s /home/jesmasta/amu/neurosci/ds105 z
+sudo ln -s /home/jesmasta/git/eeg_01_mwm_blink w
