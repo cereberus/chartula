dbc18121683f5a72a4062adb65c17a25be9a2083
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-03-02 00:46:41 +0100

    split_sub002.py up to model_rin_files list fill

--- python/neurosci/split_volumes_cond/split_sub002.py
+++ python/neurosci/split_volumes_cond/split_sub002.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import svc
 import numpy as np
 
 num_of_runs = 12
@@ -10,27 +11,23 @@ num_of_conditions = 8
 # bold_cond_list = [[[0]*2] * num_of_conditions] * num_of_runs
 bold_cond_list = np.zeros((num_of_runs, num_of_conditions, 2))
 
+model_run_files = svc.get_model_run_files()
+
 while (len(model_run_files) > 0):
 
     # take first element of the list of condition files
     considered = model_run_files.pop(0)
 
     # get specification, an info about runs and conds numbers
-    run_num, cond_num = get_file_spec(considered)
+    run_num, cond_num = svc.get_file_spec(considered)
 
     # read the file for cond info for the particular run
     # TODO cond is a 2-element list - range: from x to y second
-    cond_info = get_cond_info(considered)
+    cond_info = svc.get_cond_info(considered)
 
     # append the info about conditon to the main list with the
     # info about the bolds
     # -1 beacause in the files numbering starts with 1, not 0
     bold_cond_list[run_num-1][cond_num-1] = cond_info
-    
 
 
-
-
-
-
-    
