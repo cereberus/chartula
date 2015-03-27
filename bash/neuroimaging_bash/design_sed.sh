3b4f66798ef3d977c968feafc61b93932513615e
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-03-27 12:50:52 +0100

    bash/design_sed major changes alpha

--- bash/neuroimaging_bash/design_sed.sh
+++ bash/neuroimaging_bash/design_sed.sh
@@ -9,28 +9,27 @@
 #
 ############################################
 
-img_dirs=$(ls -d -1 */ )
+
+img_dirs=$(ls -d -1 cocaine/*/)
+img_dirs+=$(ls -d -1 controls/*/)
+
+echo $img_dirs
 
 main_design='design.fsf'
-# ROOT='/home/jesmasta/neuro/'
 
-sub_base='9603424'
-num='1'
+sub_base='1065809'
+# sub='9858555'
+# num='2'
 
+mkdir designs
 
 echo $ROOT$sub'session_1/_rest_'$num'/rest_linearMNI3mm.nii.gz'
-
 echo $img_dirs
 
 for sub in $img_dirs
 do
     for num in 1 2 
     do
-        sed "s/$sub_base rest_1/rest_$num/g" design.fsf > designs/design_${sub}_rest_${num}.fsf
+        sed "s/${sub_base}\/session_1\/rest_1/${sub}\/session_1\/rest_$num/g" design.fsf > designs/design_${sub}_rest_${num}.fsf
     done
 done
-
-    sed 's/rest_1/rest_2/g' design.fsf > designs/design_${sub}_rest_${num}.fsf
-
-sed 's/rest_1/rest_2/g' design.fsf > ../../../../designs/design_9603424_rest_2.fsf
-
