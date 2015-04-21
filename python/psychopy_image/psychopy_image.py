8c835e0122f805412d5afc5aa0f471e330944ae2
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-04-21 18:10:34 +0200

    python/psychopy_image init commit

---
+++ python/psychopy_image/psychopy_image.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+
+from psychopy import visual, core, event, gui
+# import random
+# import datetime
+# import csv
+# import os
+# from os import listdir
+# from os.path import isfile, join
+
+mywin = visual.Window([2560, 1440],monitor='testMonitor', \
+        winType='pyglet', units='pix', fullscr = True)
+mywin.setMouseVisible(False)
+
+image = visual.ImageStim(win=mywin, image='cee.png', pos=(0,0))
+
+image.draw()
+mywin.flip()
+
+while not 'space' in event.getKeys():
+    pass
+
+mywin.close()
