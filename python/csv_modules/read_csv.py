bba71d8d70180bf0c875b1448794fd5c0c3751b8
Mikolaj Buchwald <mikolaj.buchwald@gmail.com>
2015-06-05 12:54:22 +0200

    python/csv_modules read advanced function

--- python/csv_modules/read_csv.py
+++ python/csv_modules/read_csv.py
@@ -6,7 +6,8 @@
     # # #
 
     arguments:
-        input_csv - read csv file name
+        csv_file - read csv file name
+        coma - if True replaces all the comas with dots in the particular cells
         header - specify how many lines (from the begining) are to be removed
     # # #
 
@@ -22,12 +23,77 @@
 import csv
 
 
-def read(csv_file, mode='rb', delimiter=',', quotechar='|', header=None):
+def read(
+        csv_file,
+        comas=True,
+        delimiter=',',
+        header=None,
+        mode='rb',
+        quotechar='|'
+        ):
+
     data = []
-    with open(csv_file, mode) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
-        for row_list in csv_reader:
-            data.append(row_list)
+
+    print('init')
+    if len(delimiter) > 1:
+        data = advanced(
+            csv_file,
+            comas=comas,
+            data=data,
+            delimiter=delimiter,
+            mode=mode)
+    else:
+        data = basic(
+            csv_file=csv_file,
+            comas=comas,
+            data=data,
+            delimiter=delimiter,
+            mode=mode,
+            quotechar=quotechar
+            )
     if header is not None:
         data = data[header:]
+    return data
+
+
+def basic(
+        csv_file,
+        data,
+        comas=False,
+        delimiter=',',
+        mode='rb',
+        quotechar='|'
+        ):
+    with open(csv_file, mode) as csv_file:
+        csv_reader = csv.reader(
+            csv_file,
+            delimiter=delimiter,
+            quotechar=quotechar
+            )
+        for row_list in csv_reader:
+            if comas:
+                # change comas to dots
+                row_list = [i.replace(',', '.') for i in row_list]
+            data.append(row_list)
+    return data
+
+
+def advanced(
+        csv_file,
+        data,
+        delimiter=', ',
+        comas=True,
+        mode='rb'
+        ):
+
+    with open(csv_file, mode) as f:
+        for line in f:
+            # remove new line character
+            line = line.rstrip('\n')
+            # split to cells accordingly to the double delimiter
+            line = line.split(delimiter)
+            if comas:
+                # change comas to dots
+                line = [i.replace(',', '.') for i in line]
+            data.append(line)
     return data
